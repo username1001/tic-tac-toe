82eab4cc9b28e681be4c7a754cb90d9fd75d8e96
Matt <Faymatt96@gmail.com>
2017-09-10 02:15:45 -0400

    Made class component a function

--- tictac/src/components/Square.js
+++ tictac/src/components/Square.js
@@ -1,19 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
+const Square = (props) => {
+  return (
+    <button className="square" onClick={() => props.onClick}>
+      {props.value}
+    </button>
+  );
+};
 
 export default Square;
